152e2ece3bcb8b8f7a0a8980fd4f3a254544eb30
Mike Mwanje <mwanjemike767@gmail.com>
2020-09-30 21:50:25 +0300

    Shopping cart complete

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
 var shoppingCart = (function () {
-	function shoppingCart() {
-		this.items = []
-	}
-	shoppingCart.prototype.addItem = function (item) {
-		this.items.push(item)
-	}
-	shoppingCart.prototype.removeItem = function (item) {
-		var index = this.items.indexOf(item)
-		if (index > -1) {
-			this.items.splice(index, 1)
-		}
-	}
-	shoppingCart.prototype.showItems = function () {
-		for (var i in this.items) {
-			console.log('' + this.items[i])
-		}
-	}
-	shoppingCart.prototype.getTotalAmount = function () {
-	}
-	shoppingCart.prototype.checkout = function () {
-	}
-	return shoppingCart
-})()
-var mine = new shoppingCart()
-mine.addItem('Apple')
-mine.addItem('Banana')
-mine.showItems()
-mine.removeItem('Apple')
-mine.showItems()
+    function shoppingCart(list) {
+        this.items = [];
+        this.prices = [];
+        this.totalItems = 0;
+        this.totalAmount = 0;
+        if (shoppingCart.instances !== 0) {
+            return shoppingCart.current;
+        }
+        else {
+            shoppingCart.instances = 1;
+            shoppingCart.current = this;
+            this.addMany(list);
+        }
+    }
+    shoppingCart.prototype.addPrice = function (price) {
+        this.prices.push(price);
+    };
+    shoppingCart.prototype.addItem = function (item) {
+        this.items.push(item);
+    };
+    shoppingCart.prototype.add = function (item, price) {
+        this.addItem(item);
+        this.addPrice(price);
+        this.totalItems += 1;
+    };
+    shoppingCart.prototype.addMany = function (list) {
+        for (var i = 0; i < list.length; i += 2) {
+            var item = list[i];
+            var price = list[(i + 1)];
+            this.add(item, price);
+        }
+    };
+    shoppingCart.prototype.removePrice = function (index) {
+        this.prices.splice(index, 1);
+    };
+    shoppingCart.prototype.removeItem = function (index) {
+        this.items.splice(index, 1);
+    };
+    shoppingCart.prototype.remove = function (item) {
+        var index = this.items.indexOf(item);
+        if (index > -1) {
+            this.removeItem(index);
+            this.removePrice(index);
+            this.totalItems -= 1;
+        }
+    };
+    shoppingCart.prototype.showItems = function () {
+        return this.items;
+    };
+    shoppingCart.prototype.getTotalAmount = function () {
+        for (var i in this.prices) {
+            this.totalAmount += this.prices[i];
+        }
+        return this.totalAmount;
+    };
+    shoppingCart.prototype.checkout = function () {
+        console.log('Checkout!');
+        console.log("Items: " + this.items);
+        console.log("Total Items: " + this.totalItems);
+        console.log("Total amount: " + this.getTotalAmount());
+    };
+    shoppingCart.instances = 0;
+    return shoppingCart;
+})();
+var mine = new shoppingCart(['Apple', 2000, 'Banana', 2000, 'Orange', 1000]);
+mine.checkout();
+// mine.add('Apple', 1000);
+// mine.add('Banana', 2000);
+mine.add('Pineapple', 2000);
+mine.checkout();
--- app.ts
+++ app.ts
@@ -1,32 +1,54 @@
 class shoppingCart {
     items: any[] = [];
-    totalItems: number;
-    totalAmount: number;
-
-    addItem(item) {
+    prices: number[] = [];
+    totalItems: number = 0;
+    totalAmount: number = 0;
+    
+    private addPrice(price){
+        this.prices.push(price);        
+    }
+    private addItem(item) {
         this.items.push(item);
     }
-    removeItem(item) {
+    add(item, price) {
+        this.addItem(item);
+        this.addPrice(price);
+        this.totalItems += 1;
+    }
+
+    private removePrice(index){        
+        this.prices.splice(index, 1);
+    }
+    private removeItem(index) {
+        this.items.splice(index, 1);
+    }
+    remove(item) {
         const index = this.items.indexOf(item);
         if (index > -1) {
-          this.items.splice(index, 1);
+            this.removeItem(index);
+            this.removePrice(index);
+            this.totalItems -= 1
         }
     }
     showItems() {
-        for (var i in this.items){
-            console.log(`${this.items[i]}`);
-        }
+        return this.items;
     }
     getTotalAmount() {
-
+        for (var i in this.prices){
+            this.totalAmount += this.prices[i]
+        }
+        return this.totalAmount;   
     }
     checkout() {
-
+        console.log('Checkout!')
+        console.log(`Items: ${this.items}`);        
+        console.log(`Total Items: ${this.totalItems}`);
+        console.log(`Total amount: ${this.getTotalAmount()}`);
+        
     }
 }
 let mine = new shoppingCart()
-mine.addItem('Apple');
-mine.addItem('Banana');
-mine.showItems();
-mine.removeItem('Apple');
-mine.showItems();
+mine.add('Apple', 1000);
+mine.add('Banana', 2000);
+mine.add('Orange', 2000);
+mine.checkout()
